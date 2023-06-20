16051e310e92a4351ae3e57af7c98d44797dd392
nurdinhajo <nurudiin222@gmail.com>
2023-06-20 20:30:27 +0300

    More Ruby

--- app.rb
+++ app.rb
@@ -1,9 +1,32 @@
-puts ("Hello World")
-
-puts [1,2,3]
-
-p [1,2,3]
+# puts ("Hello World")
 
 # puts [1,2,3]
 
-pp [{id: 1, name: "Nuru"}, {id:2, name: "Jane"}]
+# p [1,2,3]
+
+# # puts [1,2,3]
+
+# pp [{id: 1, name: "Nuru"}, {id:2, name: "Jane"}]
+
+# print 123
+# print 456
+# print 123
+# p 789
+
+def add_number(numI, numII)
+ p numI + numII
+ puts numI * numII
+end
+add_number(1,2)
+
+
+
+
+# Single-line Comment
+
+=begin
+ To write 
+ Multi-line 
+ comments
+
+=end
